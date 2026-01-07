efe3cf0b5be23c1a8f61cd544738c5527c28856e
QuentinHunter <quentinhunter@icloud.com>
2026-01-07 00:04:55 +0000

    Add rewrites for static HTML pages

--- quiz/next.config.js
+++ quiz/next.config.js
@@ -3,6 +3,18 @@ const nextConfig = {
   reactStrictMode: true,
   images: {
     domains: ['quentinhunter.com', 'images.leadconnectorhq.com']
+  },
+  async rewrites() {
+    return [
+      {
+        source: '/',
+        destination: '/index.html'
+      },
+      {
+        source: '/pages/:path*',
+        destination: '/pages/:path*'
+      }
+    ]
   }
 }
 
